e5aeb5c3b4c08b4fb9a4d1060b0a1ee022f7209e
An KangMin <hkkm612@gmail.com>
2020-02-10 15:23:32 +0900

    Dynamic Component Generation & Using Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import Potato from './Potato'
+import React from "react";
 
+function Food({ name, picture }) {
+  // props 중 원하는 것을 가져오는 방법은 두가지 입니다.
+  // 1. component를 호출 할 때 props를 argument로 주고, props.fav를 통해 이용하는 방법
+  //  ex) function (props) { return <h2>I Like {props.name}</h2>;}
+  // 2. component를 호출 할 때 원하는 것을 중괄호에 넣어 직접 argument로 넘겨 주는 방법
+  //  ex) function ({name}) { return <h2>I Like {name}</h2>;}
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <Potato/>
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
