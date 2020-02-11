9812fdc1e4f973eea3c8d3c0acb6245a3e7b1b2f
An KangMin <hkkm612@gmail.com>
2020-02-11 15:38:23 +0900

    Use state and Change state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ class App extends React.Component {
   state = {
     count: 0
   };
+  /* 
+    react에서 state를 직접 변경하지 않는 이유는 render를 거치지 않아 화면상에 변경내용을 보일 수 없기 때문 입니다.
+    이를 해결하기위해서 setState를 이용하면 변화가 생길 때 마다 자동으로 render를 하여 화면에 적용하게 됩니다.
+    즉 setState를 호출할 때 마다 render가 반복됩니다.
+  */
   add = () => {
-    console.log("add");
+    this.setState(current => ({ count: current.count + 1 }));
+    /*
+      this.setState({this.state.count + 1}); 을 이용해도 같은 결과를 보여줍니다. 하지만 계속해서 state에 의존하게 되고,
+      이 외부 상태에 의존하지 않기 위해서 current를 활용하는 방법이 더 좋은 방법입니다.
+    */
   };
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({ count: current.count - 1 }));
   };
   render() {
     return (
