d573b19f0cba594608109ed0fa562ebca7709d3c
An KangMin <hkkm612@gmail.com>
2020-02-12 20:31:03 +0900

    Rendering Movies with data from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 /* 
 https://www.npmjs.com/package/axios 에서 axios에 관한 것을 알 수 있습니다.
 axios는 네트워크로부터 데이터를 요청 할 수 있게 하는 package 입니다.
 */
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
   getMovies = async () => {
     // 해당 함수는 axios를 이용해 url으로 부터 데이터를 가져옵니다.
     // 이 과정은 시간이 걸리기 때문에 완료될 때 까지 시간이 필요하여
     // async라는 키워드를 통해 함수가 비동기 함수임을 알리고,
     // await 키워드를 통해 기다려야할 대상이 무엇인지 알려주고 있습니다.
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    // 해당 url에서 가져온 data에서 우리가 필요로 하는 data의 위치를 맞추기 위해 변수를 위와 같이 설정했다.
+
+    this.setState({ movies, isLoading: false });
+    // setState에서 state에 있는 변수 이름과 해당 변수와 대응 시킬 변수의 이름이 같다면
+    // setState({movies:movies}) 대신에 setState({movies})를 사용할 수 있다.
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
