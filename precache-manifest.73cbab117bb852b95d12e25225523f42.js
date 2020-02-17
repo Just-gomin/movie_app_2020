3f109ed9540fda612a23e691c13fe10fcf9a35bc
An KangMin <hkkm612@gmail.com>
2020-02-17 15:54:34 +0900

    Updates

---
+++ precache-manifest.73cbab117bb852b95d12e25225523f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568bf6d18131e073ff19d7e1925a8ba1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "18f48a3e512b725ad5c5",
+    "url": "/movie_app_2020/static/css/main.8626e2ff.chunk.css"
+  },
+  {
+    "revision": "39b8bcc7d2d72670fac8",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f48a3e512b725ad5c5",
+    "url": "/movie_app_2020/static/js/main.48c9b41c.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
